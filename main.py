cbc2cf023187d84da94fd5293697a1acdde532a4
SaadNabeel <saadnabeel2010@gmail.com>
2024-10-06 14:48:26 +0500

    intial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+
+emptylist = []
+print("Empty list:", emptylist)
+
+
+listwithelements = [1, 2, 3, 4, 5]
+print("List with elements:", listwithelements)
+
+
+repeatedlist = listwithelements * 3  
+print("List repeated 3 times:", repeatedlist)
+
+
+reversedlist = listwithelements[::-1]
+print("Reversed list:", reversedlist)
